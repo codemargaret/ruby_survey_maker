7299f299b06cbcaac70345bac87bdce8092c57fa
Dan Kiss and Margaret Berry <dan@>
2017-09-26 11:34:58 -0700

    set up individual survey page

--- app.rb
+++ app.rb
@@ -11,3 +11,31 @@ get('/') do
   @surveys = Survey.all()
   erb(:index)
 end
+
+post("/") do
+  title = params["title"]
+  Survey.create({:title => title})
+  redirect "/"
+end
+
+get('/surveys/:id') do
+  @questions = Question.all()
+  id = params[:id].to_i
+  # binding.pry
+  @survey = Survey.find(id)
+  # binding.pry
+  erb(:survey)
+end
+
+post('/surveys/:id') do
+  @id = params[:id].to_i
+  @questions = Question.all()
+  # binding.pry
+  @survey = Survey.find(@id)
+  survey_id = @survey.id
+  description = params.fetch("description")
+  Question.create({:description => description, :survey_id => survey_id})
+  # binding.pry
+  # @questions = Question.all()
+  erb(:survey)
+end
